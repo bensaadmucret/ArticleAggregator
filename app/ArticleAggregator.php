e833a1a10a5a954e978d749573e509165b45ab28
Ben <bensaadmucret@gmail.com>
2023-04-27 19:55:39 +0200

    add structure && class

---
+++ app/ArticleAggregator.php
@@ -0,0 +1,74 @@
+<?php
+
+
+declare (strict_types=1);
+
+namespace Alltricks;
+
+use PDO;
+use Iterator;
+
+final class ArticleAggregator implements Iterator
+{
+    private array $articles = [];
+    private int $position = 0;
+    private PDO $db;
+
+    public function __construct()
+    {
+        $dbConnexion = new dbConnexion();
+        $this->db = $dbConnexion->getPdo();
+    }
+
+    public function appendDatabase(int $sourceId): void
+    {
+        $stmt = $this->db->prepare("SELECT article.name, article.content, source.name AS sourceName FROM article JOIN source ON article.source_id = source.id WHERE source.id = ?");
+        $stmt->execute([$sourceId]);
+
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+            $this->articles[] = (object) [
+                'name' => $row['name'],
+                'content' => $row['content'],
+                'sourceName' => $row['sourceName']
+            ];
+        }
+    }
+
+    public function appendRss(string $sourceName, string $feedUrl): void
+    {
+        $xml = simplexml_load_file($feedUrl);
+
+        foreach ($xml->channel->item as $item) {
+            $this->articles[] = (object) [
+                'name' => (string) $item->title,
+                'content' => (string) $item->description,
+                'sourceName' => $sourceName
+            ];
+        }
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    public function current(): object
+    {
+        return $this->articles[$this->position];
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function next(): void
+    {
+        ++$this->position;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->articles[$this->position]);
+    }
+}
---
+++ public/ressources/index.php
@@ -0,0 +1,59 @@
+<?php
+
+use Alltricks\ArticleAggregator;
+
+// Inclusion de l'autoloader de composer pour charger les classes automatiquement
+require __DIR__ . '/../vendor/autoload.php';
+
+// Initialisation de la classe ArticleAggregator
+$aggregator = new ArticleAggregator('localhost', 'root', '', 'my_database');
+
+// Ajout des sources RSS à l'agrégateur
+$aggregator->appendRss('Le Monde', 'https://www.lemonde.fr/rss/une.xml');
+$aggregator->appendRss('BBC News', 'https://feeds.bbci.co.uk/news/rss.xml');
+$aggregator->appendRss('CNN', 'http://rss.cnn.com/rss/edition.rss');
+
+// Ajout des sources de la base de données à l'agrégateur
+$aggregator->appendDatabase(1);
+$aggregator->appendDatabase(2);
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- style -->
+    <link rel="stylesheet" href="ressources/style.css">
+    <title>Titre de la page</title>
+</head>
+
+<body>
+    <header>
+        <!-- En-tête du site -->
+    </header>
+
+    <main>
+        <section>
+            <h2>Derniers articles</h2>
+            <?php foreach ($aggregator as $article): ?>
+            <div class="card">
+                <div class="card-header">Titre de la carte</div>
+                <div class="card-body">
+                    <?php echo '<h2>' . $article->name . '</h2>'; ?>
+                    <?php echo '<p>' . $article->content . '</p>'; ?>
+                </div>
+                <div class="card-footer"><?php echo '<p><em>Source: ' . $article->sourceName . '</em></p>'; ?></div>
+            </div>
+            <?php endforeach ?>
+        </section>
+    </main>
+
+    <footer>
+    
+    </footer>
+</body>
+
+</html>
